d6d283749a429c097cfee0dec4ab92cde5bff400
Vince Varga <vince.varga@dartside.dev>
2022-08-02 11:18:34 +0200

    Use expectPrint to automatically verify example prints
    Closes #3

--- example/pop_example.dart
+++ example/pop_example.dart
@@ -1,31 +1,30 @@
 import 'package:pop/pop.dart';
-import 'package:collection/collection.dart' hide ListExtensions;
 
 void main() {
   // reverse
   final reverseList = [4, 3, 2, 1];
   reverseList.reverse();
-  print(reverseList); // [1, 2, 3, 4]
+  expectPrint(reverseList, '[1, 2, 3, 4]');
 
   // reverse with range
   final reverseWithRangeList = [0, 1, 5, 4, 3, 2, 6, 7];
   reverseWithRangeList.reverse(2, 6);
-  print(reverseWithRangeList); // [0, 1, 2, 3, 4, 5, 6, 7]
+  expectPrint(reverseWithRangeList, '[0, 1, 2, 3, 4, 5, 6, 7]');
 
   // splitReverse
   final splitReverseList = [-2, -1, /* split here */ 4, 3, 2, 1];
   splitReverseList.splitReverse(2);
-  print(splitReverseList); // [-1, -2, 1, 2, 3, 4]
+  expectPrint(splitReverseList, '[-1, -2, 1, 2, 3, 4]');
 
   // rotate
   final rotateList = [1, 2, 3, 4];
   rotateList.rotate(1);
-  print(rotateList); // [4, 1, 2, 3]
+  expectPrint(rotateList, '[4, 1, 2, 3]');
 
   // rotate
   final anotherList = [1, 2, 3, 4];
-  print(anotherList.rotated(1).take(3).toList()); // [4, 1, 2]
-  print(anotherList); // [1, 2, 3, 4];
+  expectPrint(anotherList.rotated(1).take(3).toList(), '[4, 1, 2]');
+  expectPrint(anotherList, '[1, 2, 3, 4]'); //
 
   // range
   for (final v in range(1, 3)) {
@@ -37,30 +36,30 @@ void main() {
   // swap
   final swappedList = [0, 1, 2, 3];
   swappedList.swap(1, 3);
-  print(swappedList); // [0, 3, 2, 1]
+  expectPrint(swappedList, '[0, 3, 2, 1]');
 
   // mSwap
   final mSwappedList = [0, 1, 2, 3];
   mSwappedList.mSwap(0, -1);
-  print(mSwappedList); // [3, 1, 2, 0]
+  expectPrint(mSwappedList, '[3, 1, 2, 0]');
 
   // times
   final timesList = ['a', 'b', 'c'];
   2.times(timesList.removeLast);
-  print(timesList); // ['a']
+  expectPrint(timesList, '[a]');
 
   // timesIndexed
   var counter = 0;
   4.timesIndexed((i) => counter += i); // 0 + 1 + 2 + 3
-  print(counter); // 6
+  expectPrint(counter, '6');
 
   // removeFirst and addFirst
   final firstList = [0, 1, 2];
   final removedFirstValue = firstList.removeFirst();
-  print(removedFirstValue); // 0
-  print(firstList); // [1, 2]
+  expectPrint(removedFirstValue, '0');
+  expectPrint(firstList, '[1, 2]');
   firstList.addFirst(removedFirstValue);
-  print(firstList); // [0, 1, 2]
+  expectPrint(firstList, '[0, 1, 2]');
 
   // Counting letters with and without DefaultMap
   final string = 'mississippi'.split('');
@@ -72,14 +71,21 @@ void main() {
   }
   // With DefaultMap
   final counterMap2 = DefaultMap<String, int>(0);
-  for (final char in string) counterMap2[char] += 1;
-  // Both give {m: 1, i: 4, s: 4, p: 2}
-  print(counterMap1);
-  print(counterMap2);
+  for (final char in string) counterMap2[char]++;
+  expectPrint(counterMap1, '{m: 1, i: 4, s: 4, p: 2}');
+  expectPrint(counterMap2, '{m: 1, i: 4, s: 4, p: 2}');
 
   // Grouping items with DefaultMap
   final grouped = DefaultMap<String, List<int>>.generate((_) => []);
-  final entriesToBeGrouped = ['a', 'b', 'c', 'a', 'b', 'b'];
-  entriesToBeGrouped.forEachIndexed((i, e) => grouped[e]..add(i));
-  print(grouped);
+  final strings = ['a', 'b', 'c', 'a', 'b', 'b'];
+  for (var e in strings.entries) grouped[e.value].add(e.index);
+  expectPrint(grouped, '{a: [0, 3], b: [1, 4, 5], c: [2]}');
+}
+
+void expectPrint(Object? object, String expected) {
+  final actual = '$object';
+  print(actual);
+  if (actual != expected) {
+    throw AssertionError('Expected "$expected", but got $actual');
+  }
 }
